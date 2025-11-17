73d9d032c0fb9f36a1e334f71a71024d965e7ba6
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-16 21:47:25 -0300

    criado as rotas de schools

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource("/schools", SchoolController::class);
