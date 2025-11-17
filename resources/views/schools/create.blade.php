ac31f54467945c1f97db530cbca2ad0b1e49cfd6
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-16 21:56:59 -0300

    view de cadastro

---
+++ resources/views/schools/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+
+@section('title', 'cadastro')
+
+@section('headerTitle', 'Cadastro de escolas')
+
+@section('content')
+
+    @if (session('error'))
+        <div id="errorMessage">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    <form action="{{ route('schools.store') }}" method="post" id="formCreate">
+        @csrf   
+
+        <h1 id="formTitle">Cadastrar</h1>
+
+        <div class="formContainer">
+            <label for="name">Nome: </label>
+            <input type="text" name="name" class="input">
+        </div>
+
+        <div class="formContainer">
+            <label for="cnpj">CNPJ: </label>
+            <input type="number" name="CNPJ" class="input">
+        </div>
+
+        <div class="formContainer">
+            <label for="email">email: </label>
+            <input type="number" name="email" class="input">
+        </div>
+
+        <div id="buttonContainer">
+            <div id="cancelContainer">
+                <a href="{{ route('schools.index') }}" id="cancel">Cancelar</a>
+            </div>
+
+            <input type="submit" value="Cadastrar" id="create">
+        </div>
+    </form>
+    
+@endsection
