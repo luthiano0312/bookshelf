cf93c1c601314abee2179d31abbf16d3df40a668
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-18 19:47:01 -0300

    adicionado o relacionamento nas models

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function escola()
+    {
+        return $this->belongsTo(Escola::class, 'school_id');
+    }
+
 }
