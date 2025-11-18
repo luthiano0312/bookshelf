7d57d4ba607b7503192bed9cc1b886f7965e3de3
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-18 19:45:54 -0300

    renomeando a migration de users e adicionado a Fk school_id

--- database/migrations/2026_01_01_000000_create_users_table.php
+++ database/migrations/2026_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('role');
             $table->string('email')->unique();
+            $table->foreignId('school_id')->constrained()->onDelete('cascade'); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
