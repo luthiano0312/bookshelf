027bb84f8d3ac64e2e7a173f0c8f3bb451946845
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-18 21:00:46 -0300

    criado as rotas de categorias

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\categoryController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
@@ -23,3 +24,4 @@ require __DIR__.'/auth.php';
 
 Route::resource("/schools", SchoolController::class);
 Route::resource("/users", UserController::class);
+Route::resource("/categories", categoryController::class);
