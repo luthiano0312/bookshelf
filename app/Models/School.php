30dc36f7c88c6f95c121d37ca48a15bc0d425b65
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-20 12:35:04 -0300

    formatação do cnpj

--- app/Models/School.php
+++ app/Models/School.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     protected $fillable = ["cnpj","name","email"];
+
+    public function getCnpjFormattedAttribute()
+    {
+        return preg_replace(
+            '/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/',
+            '$1.$2.$3/$4-$5',
+            $this->cnpj
+        );
+    }
+
+    public function setCnpjAttribute($value)
+    {
+        $this->attributes['cnpj'] = preg_replace('/\D/', '', $value);
+    }
 }
