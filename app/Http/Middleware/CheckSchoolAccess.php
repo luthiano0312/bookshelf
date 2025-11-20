2e3599e3b47b3c9e050d25215733250e54e1e51a
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-20 19:13:48 -0300

    middleware para impedir certas ações de acordo com a escola do user

---
+++ app/Http/Middleware/CheckSchoolAccess.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckSchoolAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+
+        if ($user->role == 1) {
+            return $next($request);
+        }
+
+        $schoolIdFromRoute = $request->route('school')
+                          ?? $request->route('user')
+                          ?? $request->route('book')
+                          ?? $request->school_id
+                          ?? null;
+
+        if (!$schoolIdFromRoute) {
+            return $next($request);
+        }
+
+        if (is_object($schoolIdFromRoute)) {
+            $schoolIdFromRoute = $schoolIdFromRoute->school_id ?? $schoolIdFromRoute->id;
+        }
+
+        if ($user->school_id != $schoolIdFromRoute) {
+            abort(403, 'Você não tem permissão para acessar dados de outra escola.');
+        }
+
+        return $next($request);
+    }
+}
