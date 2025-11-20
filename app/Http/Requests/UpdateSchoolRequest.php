4e4d7b4f98e251620f0814c37e6388bdbb1f4b0c
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-20 16:10:16 -0300

    criado o form request de escolas para o metodo update

---
+++ app/Http/Requests/UpdateSchoolRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSchoolRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $id = $this->route('school');
+
+        $this->merge([
+            'cnpj' => preg_replace('/\D/', '', $this->cnpj),
+        ]);
+
+        return [
+            "name" => "required|min:3",
+            "cnpj" => "required|digits:14|unique:schools,cnpj," . $id,
+            "email" => "required|email|unique:schools,email," . $id
+        ];
+    }
+}
