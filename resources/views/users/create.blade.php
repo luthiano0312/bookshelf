102117570c060a1a2079f30be207dc8706059bac
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-20 20:15:44 -0300

    alterado o formulario de cadastro usuarios para manter os nomes apos a validação

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -18,29 +18,48 @@
         <h1 id="formTitle">Cadastrar</h1>
 
         <div class="formContainer">
+            @error ('name')
+                {{ $message }}
+            @enderror
             <label for="name">Nome: </label>
-            <input type="text" name="name" class="input">
+            <input type="text" name="name" class="input" value="{{ old('name') }}">
         </div>
 
         <div class="formContainer">
+            @error ('email')
+                {{ $message }}
+            @enderror
             <label for="email">Email: </label>
-            <input type="text" name="email" class="input">
+            <input type="text" name="email" class="input" value="{{ old('email') }}">
         </div>
 
         <div class="formContainer">
+            @error ('password')
+                {{ $message }}
+            @enderror
             <label for="password">Senha: </label>
-            <input type="password" name="password" class="input">
+            <input type="password" name="password" class="input" value="{{ old('password') }}">
         </div>
 
         <div class="formContainer">
+            @error ('role')
+                {{ $message }}
+            @enderror
             <label for="role">Cargo: </label>
-            <input type="number" name="role" class="input">
+            <select name="role">
+                <option value=1 {{ old("role") == 1 ? 'selected' : '' }} >admin</option>
+                <option value=2 {{ old("role") == 2 ? 'selected' : '' }} >gestor</option>
+                <option value=3 {{ old("role") == 3 ? 'selected' : '' }} >bibliotecario</option>
+            </select>
         </div>
 
         <div class="formContainer">
+            @error ('school_id')
+                {{ $message }}
+            @enderror
             <select name="school_id">
                 @foreach ($schools as $school)
-                    <option value="{{ $school->id }}">{{ $school->name }}</option>
+                    <option value="{{ $school->id }}" {{ old("school_id") == $school->id ? 'selected' : '' }} >{{ $school->name }}</option>
                 @endforeach
             </select>
         </div>
