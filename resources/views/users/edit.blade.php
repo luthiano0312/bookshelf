28bc7aa626065844206a6428372eeb5a42a80bb5
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-20 20:15:58 -0300

    alterado o formulario de cadastro usuarios para manter os nomes apos a validação no update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
 
-    @if (session('error'))
-        <div id="errorMessage">
-            {{ session('error') }}
-        </div>
-    @endif
+    <!-- @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            {{ $error }}
+        @endforeach
+    @endif -->
 
     <form action="{{ route('users.update', $user->id) }}" method="post" id="formCreate">
         @csrf   
@@ -19,21 +19,37 @@
         <h1 id="formTitle">editar</h1>
 
         <div class="formContainer">
+            @error ('name')
+                {{ $message }}
+            @enderror
             <label for="name">Nome: </label>
             <input type="text" name="name" class="input" value="{{ $user->name }}">
         </div>
 
         <div class="formContainer">
+            @error ('email')
+                {{ $message }}
+            @enderror
             <label for="email">Email: </label>
             <input type="text" name="email" class="input" value="{{ $user->email }}">
         </div>
 
         <div class="formContainer">
+            @error ('role')
+                {{ $message }}
+            @enderror
             <label for="role">Nivel: </label>
-            <input type="number" name="role" class="input" value="{{ $user->role }}">
+            <select name="role">
+                <option value=1 {{ $user->role == 1 ? 'selected' : '' }} >admin</option>
+                <option value=2 {{ $user->role == 2 ? 'selected' : '' }} >gestor</option>
+                <option value=3 {{ $user->role == 3 ? 'selected' : '' }} >bibliotecario</option>
+            </select>
         </div>
 
         <div class="formContainer">
+            @error ('school_id')
+                {{ $message }}
+            @enderror
             <select name="school_id">
                 @foreach ($schools as $school)
                     <option value="{{ $school->id }}" {{ $user->school_id == $school->id ? 'selected' : '' }} >{{ $school->name }}</option>
