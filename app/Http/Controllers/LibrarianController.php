4c36a62fa5c014dd63e6363d7bdd6e959dcd6053
Luthiano Miguel <luthianomiguel@gmail.com>
2025-11-21 06:16:44 -0300

    criado LibrarianController com todos os metodos implementados

---
+++ app/Http/Controllers/LibrarianController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LibrarianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $librarians = User::all();
+        return view("librarians.index", compact("librarians"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $schools = school::all();
+        return view("librarians.create", compact("schools"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $created = User::create($request->validated());
+        
+        if ($created) {
+            return redirect()->route("librarians.index")->with("success","cadastrado com sucesso");
+        }
+       
+        return redirect()->route("librarians.create")->with("error","erro ao cadastrar");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $librarian = User::findOrFail($id);
+        $schools = school::all();
+        return view("librarians.edit", compact("librarian"), compact("schools"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, string $id)
+    {
+        $librarian = User::findOrFail($id);
+        $updated = $librarian->update($request->validated());
+
+        if ($updated) {
+            return redirect()->route("librarians.index")->with("success","atualizado com sucesso");
+        }
+        
+        return redirect()->route("librarians.edit")->with("error","erro ao atualizar");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $librarian = User::findOrFail($id);
+        $librarian->delete();
+        return redirect()->route("librarians.index");
+    }
+}
